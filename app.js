25131106567eedc38097ceca6ab15b2224361739
aghumnar <ashish_ghumnar_tp@bmc.com>
2017-09-24 21:53:29 +0530

    modified changes

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 'use strict';
 
 const express = require('express'),
-    socketIO = require('socket.io'),
-    app = express();
+    app = express(),
+    server = require('http').Server(app),
+    socketIO = require('socket.io')(server);
 
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.use(express.static(__dirname + '/client'));
 
-app.listen(9009, function () {
-    console.log('started on ..%s', 9009);
+console.log("%s %s", server_port, server_ip_address);
+
+server.listen(server_port, server_ip_address, function () {
+    console.log("Listening on " + server_ip_address + ", port " + server_port)
 });
 
-socketIO.on('connect', function () {
-
+socketIO.on('connection', function (socket) {
+    socket.emit('news', {test: 'ashis'});
 });
