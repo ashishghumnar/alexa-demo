662442ed3aaf54a57eb3b1e12e581e51dfad70a7
aghumnar <ashish_ghumnar_tp@bmc.com>
2017-09-24 22:52:08 +0530

    change proces env

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const express = require('express'),
     server = require('http').Server(app),
     socketIO = require('socket.io')(server);
 
-const server_port = process.env.PORT || 8080;
-const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.use(express.static(__dirname + '/client'));
-
+app.get('/itemCheck', function (req, res) {
+    res.send('Hey i am started!');
+});
 console.log("%s %s", server_port, server_ip_address);
 
-server.listen(server_port, function () {
-    console.log("Listening on  port " + server_port)
-});
+server.listen(server_port, server_ip_address);
+console.log('Server running on http://%s:%s', server_ip_address, server_port);
 
 socketIO.on('connection', function (socket) {
     socket.emit('news', {test: 'ashis'});
