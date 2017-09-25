7505247251653b52d76dec26feceaa18b51681c5
aghumnar <ashish_ghumnar_tp@bmc.com>
2017-09-25 13:41:00 +0530

    change ip

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 808
 const server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.use(express.static(__dirname + '/client'));
+
 app.get('/itemCheck', function (req, res) {
-    res.send('Hey i am started!');
+    res.send('Hey i am on!');
 });
+
 console.log("%s %s", server_port, server_ip_address);
 
-server.listen(server_port, server_ip_address);
+server.listen(server_port);
 console.log('Server running on http://%s:%s', server_ip_address, server_port);
 
 socketIO.on('connection', function (socket) {
