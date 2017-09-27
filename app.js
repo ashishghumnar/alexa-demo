acd92cfac371578687989076fcbbb5f17f39ddcf
aghumnar <ashish_ghumnar_tp@bmc.com>
2017-09-27 21:36:14 +0530

    adding alex code

--- app.js
+++ app.js
@@ -5,19 +5,56 @@ const express = require('express'),
     server = require('http').Server(app),
     socketIO = require('socket.io')(server);
 
+const bodyParser = require('body-parser');
+
+
 const server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.use(express.static(__dirname + '/client'));
 
-app.get('/itemCheck', function (req, res) {
-    res.send('Hey i am on!');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
+
+var sampleRespJSON = {
+    "version": "1.0",
+    "response": {
+        "outputSpeech": {
+            "type": "PlainText",
+            "text": "Hey, Hi This is Ashish"
+        },
+        "card": {
+            "type": "Simple",
+        },
+        "reprompt": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": "Can I help you with anything else?"
+            }
+        },
+        "shouldEndSession": false
+    }
+};
+
+app.post('/api.echo', function (req, res) {
+
+    var intend = getIntend(req.body.request);
+
+    if (intend === 'CreateTicketIntend') {
+        sampleRespJSON.response.outputSpeech.text = 'Let me create ticket for you';
+        //credtre
+        res.send(sampleRespJSON);
+    } else {
+        sampleRespJSON.response.outputSpeech.text = 'Hello There';
+        res.send(sampleRespJSON);
+    }
 });
 
-console.log("%s %s", server_port, server_ip_address);
+function getIntend(request) {
+    return request.intent.name;
+}
 
 server.listen(server_port);
-console.log('Server running on http://%s:%s', server_ip_address, server_port);
 
 socketIO.on('connection', function (socket) {
     socket.emit('news', {test: 'ashis'});
